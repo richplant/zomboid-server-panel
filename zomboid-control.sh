11ee5da85ca5ed716631b5fde921a413043826b6
RP <repairist@gmail.com>
2016-02-08 16:30:14 +0100

    Add console logging/progress bars

--- zomboid-control.sh
+++ zomboid-control.sh
@@ -4,86 +4,141 @@ LOGFILE="$HOME/zomboid-control.log"
 STEAMCMD_DIR="$HOME/steamcmd"
 BASEDIR="$HOME/Steam/steamapps/common/Project\ Zomboid\ Dedicated\ Server/"
 
+exec 3>&1 1>>$LOGFILE 2>&1
+
 say_this()
 {
-    screen -S zomboid -p 0 -X stuff "$1^M" >> $LOGFILE 2>&1
+    screen -S zomboid -p 0 -X stuff "$1^M"
 }
 
 update()
 {
-	echo -n `date` >> $LOGFILE && echo " Running steamcmd update" >> $LOGFILE
-	bash $STEAMCMD_DIR/steamcmd.sh +login anonymous +app_update 380870 validate +quit >> $LOGFILE 2>&1
+	echo -n `date` && echo " Running steamcmd update"
+	bash $STEAMCMD_DIR/steamcmd.sh +login anonymous +app_update 380870 validate +quit
 	wait
+	echo
 }
 
 updateRunning()
 {
-	echo -n `date` >> $LOGFILE && echo " Quitting Project Zomboid" >> $LOGFILE
+	echo -n `date` && echo " Quitting Project Zomboid"
 	say_this "quit"
-	sleep 60
+	echo -ne '           (00%)\r'
+	sleep 15
+	echo -ne '##         (25%)\r'
+	sleep 15
+	echo -ne '#####      (50%)\r'
+	sleep 15
+	echo -ne '#######    (75%)\r'
+	sleep 15
+	echo -ne '########## (100%)\r'
+	echo -ne '\n'
+	echo
 
-	echo -n `date` >> $LOGFILE && echo " Running steamcmd update" >> $LOGFILE
-	bash $STEAMCMD_DIR/steamcmd.sh +login anonymous +app_update 380870 validate +quit >> $LOGFILE 2>&1
+	echo -n `date` && echo " Running steamcmd update"
+	bash $STEAMCMD_DIR/steamcmd.sh +login anonymous +app_update 380870 validate +quit
 	wait
+	echo
 
-	echo -n `date` >> $LOGFILE && echo " Restarting Project Zomboid" >> $LOGFILE
+	echo -n `date` && echo " Restarting Project Zomboid"
 	say_this "cd $BASEDIR"
 	say_this "./start-server.sh"
+	echo -ne '           (00%)\r'
+	sleep 15
+	echo -ne '##         (25%)\r'
+	sleep 15
+	echo -ne '#####      (50%)\r'
+	sleep 15
+	echo -ne '#######    (75%)\r'
+	sleep 15
+	echo -ne '########## (100%)\r'
+	echo -ne '\n'
+	echo
 }
 
 start()
 {
-	echo -n `date` >> $LOGFILE && echo " Starting screen session" >> $LOGFILE
-	screen -d -m -S zomboid >> $LOGFILE 2>&1
+	echo -n `date` && echo " Starting screen session"
+	screen -d -m -S zomboid
+	echo
 
-	echo -n `date` >> $LOGFILE && echo " Starting Project Zomboid" >> $LOGFILE
+	echo -n `date` && echo " Starting Project Zomboid"
 	say_this "cd $BASEDIR"
 	say_this "./start-server.sh"
-	sleep 60
+	echo -ne '           (00%)\r'
+	sleep 15
+	echo -ne '##         (25%)\r'
+	sleep 15
+	echo -ne '#####      (50%)\r'
+	sleep 15
+	echo -ne '#######    (75%)\r'
+	sleep 15
+	echo -ne '########## (100%)\r'
+	echo -ne '\n'
+	echo
 }
 
 stop()
 {
-	echo -n `date` >> $LOGFILE && echo " Stopping Project Zomboid" >> $LOGFILE
+	echo -n `date` && echo " Stopping Project Zomboid"
 	say_this "quit"
-	sleep 60
+	echo -ne '           (00%)\r'
+	sleep 15
+	echo -ne '##         (25%)\r'
+	sleep 15
+	echo -ne '#####      (50%)\r'
+	sleep 15
+	echo -ne '#######    (75%)\r'
+	sleep 15
+	echo -ne '########## (100%)\r'
+	echo -ne '\n'
+	echo
 
-	echo -n `date` >> $LOGFILE && echo " Stopping screen session" >> $LOGFILE
-	screen -S zomboid -X quit >> $LOGFILE 2>&1
+	echo -n `date` && echo " Stopping screen session"
+	screen -S zomboid -X quit
+	echo
 }
 
 title="Zomboid server control panel"
 prompt="Pick an option: "
 options=("Start server" "Stop server" "Update stopped server" "Update running server" "Quit")
 
-echo $title
+echo $title 1>&3
 PS3="$prompt"
 select opt in "${options[@]}"
 do
     case $opt in
         "Start server")
-            echo "Starting server..."
-            start
-            echo "Complete."
+            echo "Starting server..." | tee /dev/fd/3
+            start | tee /dev/fd/3
+            echo "Complete." | tee /dev/fd/3
+            echo
+            echo $title | tee /dev/fd/3
             ;;
         "Stop server")
-            echo "Stopping server..."
-            stop
-            echo "Complete."
+            echo "Stopping server..." | tee /dev/fd/3
+            stop | tee /dev/fd/3
+            echo "Complete." | tee /dev/fd/3
+            echo
+            echo $title | tee /dev/fd/3
             ;;
         "Update stopped server")
-            echo "Updating server..."
-            update
-            echo "Complete."
+            echo "Updating server..." | tee /dev/fd/3
+            update | tee /dev/fd/3
+            echo "Complete." | tee /dev/fd/3
+            echo
+            echo $title | tee /dev/fd/3
             ;;
         "Update running server")
-			echo "Updating server..."
-			updateRunning
-			echo "Complete."
+			echo "Updating server..." | tee /dev/fd/3
+			updateRunning | tee /dev/fd/3
+			echo "Complete." | tee /dev/fd/3
+			echo
+			echo $title | tee /dev/fd/3
 			;;
         "Quit")
             break
             ;;
         *) echo invalid option;;
     esac
-done
+done 2>&3
